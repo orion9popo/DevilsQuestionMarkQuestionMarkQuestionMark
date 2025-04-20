646bc58389e3cb65dcb3130232d385f2c0cff2c0
orion9popo <orionginthegreat@gmail.com>
2025-04-20 02:15:54 -0400

    alr lets get back to work people

--- Movement/Assets/Scripts/gui reference.cs
+++ Movement/Assets/Scripts/gui reference.cs
@@ -5,10 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class GUIreference : MonoBehaviour
 {
+    public GameObject player, camera;
+    public Transform[] viewPos;
+    public Transform[] PlayerPos;
+
     public void Begin(){
         SceneManager.LoadScene("Training");
     }
     public void End(){
         Application.Quit();
     }
+    public void SitDown(){
+        camera.transform.position = viewPos[0].position;
+        camera.transform.rotation = viewPos[0].rotation;
+        player.transform.position = PlayerPos[0].position;
+        player.transform.rotation = PlayerPos[0].rotation;
+        Animator animator = player.GetComponent<Animator>();
+        animator.SetTrigger("Sit");
+    }
+
 }
